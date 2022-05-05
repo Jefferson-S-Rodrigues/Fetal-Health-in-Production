dc26c8ab4d71c494111d5810a55d762e0b285646
Jefferson-S-Rodrigues <jefferson2jsr@gmail.com>
2022-05-05 20:30:31 -0300

    Uso de environment para distinguir APIs

--- hpeml-front/src/app/core/ctg-exam.service.ts
+++ hpeml-front/src/app/core/ctg-exam.service.ts
@@ -2,17 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Ctgexam } from '../shared/models/ctgexam';
-
-const url = 'http://localhost/api/ctgexam';
+import { ApiConfigService } from './api-config.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CtgExamService {
+export class CtgExamService extends ApiConfigService {
 
-  constructor(private http: HttpClient) { }
+  url: string = `${this.apiUrl}/ctgexam`;
+
+  constructor(private http: HttpClient) {
+    super();
+  }
 
   saveCTGExam(exam: Ctgexam): Observable<Ctgexam> {
-    return this.http.post<Ctgexam>(url, exam);
+    return this.http.post<Ctgexam>(this.url, exam);
   }
 }
